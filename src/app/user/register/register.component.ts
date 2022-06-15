e39772cb8a879fb64e5b70bfbc3e04ef9a1278f7
Andrew Alkazeer <ferasalkzer@hotmail.com>
2022-06-15 01:34:59 -0600

    designing an Alert Component #116

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -32,6 +32,9 @@ export class RegisterComponent {
     Validators.minLength(13),
     Validators.maxLength(13)
   ])
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created.'
+  alertColor = 'blue'
 
   registerForm = new FormGroup({
     name: this.name,
@@ -42,9 +45,9 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber
   })
 
-  // I am working here after revesing to the older commit 'added Alert Component'
-
   register() {
-    console.log('register called')
+    this.showAlert = true
+    this.alertMsg = 'Please wait! Your account is being created.'
+    this.alertColor = 'blue'
   }
 }
