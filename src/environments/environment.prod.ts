45bc18df228522444c651b986979a544d77b1d64
Andrew Alkazeer <ferasalkzer@hotmail.com>
2022-06-20 13:01:51 -0600

    added firebase Auth and installed firebase package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSgDc5fs6EkOCC5v8MTnZm7CzR8Sb8pzg",
+    authDomain: "clips-2380b.firebaseapp.com",
+    projectId: "clips-2380b",
+    storageBucket: "clips-2380b.appspot.com",
+    appId: "1:176458635603:web:05585320890b4a4bda29a6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCSgDc5fs6EkOCC5v8MTnZm7CzR8Sb8pzg",
+    authDomain: "clips-2380b.firebaseapp.com",
+    projectId: "clips-2380b",
+    storageBucket: "clips-2380b.appspot.com",
+    messagingSenderId: "176458635603",
+    appId: "1:176458635603:web:05585320890b4a4bda29a6",
+    measurementId: "G-78G7ST30BE"
+  }
 };
 
 /*
