e01313c34d3e318195df2835edc64940c9a8c14e
Andrew Alkazeer <ferasalkzer@hotmail.com>
2022-06-22 18:59:50 -0600

    added password match along with alert msg

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
 import IUser from 'src/app/models/user.model';
 
 @Component({
@@ -51,7 +52,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
     this.showAlert = true
