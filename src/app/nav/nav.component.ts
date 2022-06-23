d99d7403de147316442ee4764b0960a6f0d82ae4
Andrew Alkazeer <ferasalkzer@hotmail.com>
2022-06-23 00:54:43 -0600

    added manage page and modified routing #189

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -12,8 +11,7 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService,
-    private afAuth: AngularFireAuth
+    public auth: AuthService
   ) {
   }
 
@@ -24,12 +22,6 @@ export class NavComponent implements OnInit {
     $event.preventDefault()
 
     this.modal.toggleModal('auth')
-  }
-
-  async logout($event: Event) {
-    $event.preventDefault()
-
-    await this.afAuth.signOut()
   }
 
 }
